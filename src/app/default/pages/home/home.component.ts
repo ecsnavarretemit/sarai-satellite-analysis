6cb08cc1cf849eb1ad4efd2a80ce7b8f5c9b7c11
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-04-11 07:18:10 +0800

    Add default ui for the homepage

--- src/app/default/pages/home/home.component.ts
+++ src/app/default/pages/home/home.component.ts
@@ -13,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  public images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   constructor() { }
 

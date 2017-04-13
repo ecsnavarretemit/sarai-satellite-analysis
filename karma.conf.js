b71630b6c251f321d278e3fba108036d625adea3
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-04-13 13:04:38 +0800

    Serve image assets when karma runs

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,9 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    proxies: {
+      '/assets': '/base/src/assets'
+    },
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
               : ['progress', 'kjhtml'],

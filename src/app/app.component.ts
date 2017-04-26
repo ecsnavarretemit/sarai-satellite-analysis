3ab78b61543fd404ae6bc9ccc63be8ee6be36597
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-04-27 07:53:59 +0800

    Implement responsive Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@
  * Licensed under MIT
  */
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, QueryList, Renderer2, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
+  public navbarVisible = false;
+
+  @ViewChildren('dropdown') dropdowns: QueryList<ElementRef>;
+
+  constructor(private renderer: Renderer2) {  }
+
+  enableDropdown(el) {
+    // flag to determine if class should be added or not
+    const add = !el.classList.contains('open');
+
+    // add class to the element
+    if (add) {
+      this.renderer.addClass(el, 'open');
+    } else {
+      this.renderer.removeClass(el, 'open');
+    }
+  }
+
+  // prevent redirection function for dropdown anchors
+  dropdownLinkNoop(evt: Event) {
+    evt.preventDefault();
+  }
+
+  @HostListener('document:click', ['$event'])
+  documentClick(evt: Event) {
+    this.dropdowns
+      .filter((item: ElementRef) => {
+        return !item.nativeElement.contains(evt.target);
+      })
+      .forEach((item: ElementRef) => {
+        this.renderer.removeClass(item.nativeElement, 'open');
+      })
+      ;
+  }
+
 }
 
 
